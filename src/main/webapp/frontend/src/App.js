fdd59be64ae73446826dea1944109fd29f8de0c3
sleepy.joe <wildmonk1025@gmail.com>
2022-09-25 21:23:27 +0900

    testing

--- src/main/webapp/frontend/src/App.js
+++ src/main/webapp/frontend/src/App.js
@@ -1,22 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    axios.get('/home').then((res) => setData(res.data)).catch((e) => console.log(e))
+  }, [])
+
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        ></a>
+        <p>{data}</p>
       </header>
     </div>
   );
